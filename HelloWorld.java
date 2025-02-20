04e41b2631a8c7d044836621fae0106aa9a4f5ba
Himanshu Jain <himanshujain.adt@gmail.com>
2025-02-20 18:21:37 +0530

    saved file HELLOWORLD

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!"
+        System.out.println("Hello, World!");
+        
+        // Get the current date and time
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        
+        // Print the current date
+        System.out.println("Current Date and Time: " + formatter.format(date));
+    }
+}
